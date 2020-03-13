16ca8620149803b93903bf6fd3692672dd238e5a
Ben Rosen <benjamin.j.rosen@gmail.com>
2020-03-13 10:05:27 -0400

    particle fire simulator create blank window

--- break-continue.cpp
+++ break-continue.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : break-continue.cpp
-// Author      : John Purcell
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 using namespace std;
 
